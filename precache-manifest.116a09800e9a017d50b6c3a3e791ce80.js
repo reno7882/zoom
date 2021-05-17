9fcfbacf23254ec7b13d836c935f91f6b1f0cd1a
René Alejandro <reno7882@gmail.com>
2021-05-17 18:18:40 -0500

    Updates

---
+++ precache-manifest.116a09800e9a017d50b6c3a3e791ce80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f3c3609223c35fb960c",
+    "url": "/zoom/static/css/main.67a6f3af.chunk.css"
+  },
+  {
+    "revision": "3f3c3609223c35fb960c",
+    "url": "/zoom/static/js/main.2a19828b.chunk.js"
+  },
+  {
+    "revision": "45aa1f2cc61c0701fbe1",
+    "url": "/zoom/static/js/runtime~main.7f06f8b5.js"
+  },
+  {
+    "revision": "bf08d71a490fbfecc227",
+    "url": "/zoom/static/js/2.e6cc0adb.chunk.js"
+  },
+  {
+    "revision": "bc9387e41282bda7d5bd12bbc09fa02a",
+    "url": "/zoom/static/media/zoom-in.bc9387e4.svg"
+  },
+  {
+    "revision": "72523ba9c9b21e3c139148d52b8868ef",
+    "url": "/zoom/static/media/zoom-out.72523ba9.svg"
+  },
+  {
+    "revision": "fc113484ddcbc6817612b8e912878734",
+    "url": "/zoom/static/media/zoom-reset.fc113484.svg"
+  },
+  {
+    "revision": "9dac1628c48e7c65a08ae71b1103448d",
+    "url": "/zoom/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/zoom/precache-manifest.116a09800e9a017d50b6c3a3e791ce80.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/zoom/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
